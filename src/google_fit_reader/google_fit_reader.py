bb01dc8e2558f6897c0156ab5ad192332be2ff98
BlairG <blairg23@gmail.com>
2021-09-08 14:28:28 -0600

    Output file is working with JSON data.

--- src/google_fit_reader/google_fit_reader.py
+++ src/google_fit_reader/google_fit_reader.py
@@ -14,6 +14,7 @@ def main():
         directory=args.directory,
         file_type=args.file_type,
         output_filename=args.output_filename,
+        verbose=args.verbose,
     )
     google_fit_reader.parse()
 
@@ -38,6 +39,7 @@ def parse_args(args):
         "-f",
         type=str,
         default="json",
+        choices=['tcx', 'json'],
         help="Specify the type of file to parse.",
     )
     parser.add_argument(
@@ -47,80 +49,112 @@ def parse_args(args):
         default="google_fit.csv",
         help="Specify the name of the output file.",
     )
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        type=bool,
+        default=False,
+        choices=[True, False],
+        help="Specify verbosity of the output."
+    )
     return parser.parse_args(args)
 
 
 class GoogleFitReader:
-    def __init__(self, directory, file_type, output_filename):
+    def __init__(self, directory, file_type, output_filename, verbose=False):
         self._directory = directory
         self._file_type = file_type
         self._file_regex = "*." + file_type
         self._glob_path = os.path.join(self._directory, self._file_regex)
         self._output_filename = output_filename
+        self._verbose = verbose
 
     def parse(self):
-        if self._file_type.lower() == 'json':
+        if self._file_type.lower() == "json":
             self._parse_json()
-        elif self._file_type.lower() == 'tcx':
+        elif self._file_type.lower() == "tcx":
             self._parse_xml()
 
     def _parse_json(self):
+        headers = ["timestamp", "activity", "distance (miles)", "duration (min)"]
+        data = {}
+
         for filepath in glob.iglob(self._glob_path): 
-            print("filepath: ", filepath)
-            with open(filepath) as in_file:
-                activity = json.load(in_file)
-            activity_name = activity.get('fitnessActivity')
-            timestamp = activity.get('startTime')
-            total_time_seconds = float(activity.get('duration').replace('s', ''))
-            total_time_minutes = total_time_minutes = total_time_seconds / 60.0
-            aggregate = activity.get('aggregate')
+            with open(filepath) as infile:
+                activity = json.load(infile)
+            activity_name = activity.get("fitnessActivity")
+            timestamp = activity.get("startTime")
+            total_time_seconds = float(activity.get("duration").replace("s", ""))
+            total_time_minutes = str(total_time_seconds / 60.0)
+            aggregate = activity.get("aggregate")
             for element in aggregate:
-                if element.get('metricName') == 'com.google.distance.delta':
-                    distance_meters = element.get('floatValue')
-                    distance_miles = distance_meters / 1609.0
-            print('timestamp: ', timestamp)
-            print('activity: ', activity_name)
-            print('distance in miles: ', distance_miles)
-            print('total time in minutes: ', total_time_minutes)
-            print('-----')
-            sys.exit()
+                if element.get("metricName") == "com.google.distance.delta":
+                    distance_meters = element.get("floatValue")
+                    distance_miles = str(distance_meters / 1609.0)
+            
+            if self._verbose:
+                print("filepath: ", filepath)
+                print("timestamp: ", timestamp)
+                print("activity: ", activity_name)
+                print("distance in miles: ", distance_miles)
+                print("total time in minutes: ", total_time_minutes)
+                print("-----")
+
+            if timestamp in data:
+                raise Exception(f"ERROR: Duplicate timestamps: {timestamp}")
+            else:
+                data[timestamp] = {
+                    "activity": activity_name,
+                    "distance": distance_miles,
+                    "duration": total_time_minutes,
+                }
+        self._write_csv(headers=headers, data=data)
     
     def _parse_xml(self):
         # for filename in os.listdir(self._directory):
-        #     print('filename: ', filename)
+        #     print("filename: ", filename)
+        def get_namespace(element):
+            m = re.match(r"\{.*\}", element.tag)
+            return m.group(0) if m else ""
 
-        for filepath in glob.iglob(self._glob_path): 
-            print("filepath: ", filepath)
+        for filepath in glob.iglob(self._glob_path):
             tree = ET.parse(filepath)
-            # print('tree: ', tree)
+            # print("tree: ", tree)
             root = tree.getroot()
-            # print('root: ', root)
-            # print('dir(root): ', dir(root))
-            # print('root.attrib: ', root.attrib)
-            # print('root.items: ', root.items())
-            # print('root.keys: ', root.keys())
-            # print('root.tag: ', root.tag)
-            # print('root.namespace: ', self._get_namespace(element=root))
-            namespace = self._get_namespace(element=root)
+            # print("root: ", root)
+            # print("dir(root): ", dir(root))
+            # print("root.attrib: ", root.attrib)
+            # print("root.items: ", root.items())
+            # print("root.keys: ", root.keys())
+            # print("root.tag: ", root.tag)
+            # print("root.namespace: ", self._get_namespace(element=root))
+            namespace = get_namespace(element=root)
 
             # print(ET.dump(root))
-            for activities in root.iter(namespace + 'Activities'):
-                for activity in activities.iter(namespace + 'Activity'):
-                    activity_name = activity.attrib.get('Sport')
-                    timestamp = activity.find(namespace + 'Id').text
-                    lap = activity.find(namespace + 'Lap')
-                    distance_meters = float(lap.find(namespace + 'DistanceMeters').text)
-                    distance_miles = distance_meters / 1609.0
-                    total_time_seconds = float(lap.find(namespace + 'TotalTimeSeconds').text)
-                    total_time_minutes = total_time_seconds / 60.0
-                    print('timestamp: ', timestamp)
-                    print('activity: ', activity_name)
-                    print('distance in miles: ', distance_miles)
-                    print('total time in minutes: ', total_time_minutes)
-                    print('-----')
+            for activities in root.iter(namespace + "Activities"):
+                for activity in activities.iter(namespace + "Activity"):
+                    activity_name = activity.attrib.get("Sport")
+                    timestamp = activity.find(namespace + "Id").text
+                    lap = activity.find(namespace + "Lap")
+                    distance_meters = float(lap.find(namespace + "DistanceMeters").text)
+                    distance_miles = str(distance_meters / 1609.0)
+                    total_time_seconds = float(lap.find(namespace + "TotalTimeSeconds").text)
+                    total_time_minutes = str(total_time_seconds / 60.0)
+                    if self._verbose:
+                        print("filepath: ", filepath)
+                        print("timestamp: ", timestamp)
+                        print("activity: ", activity_name)
+                        print("distance in miles: ", distance_miles)
+                        print("total time in minutes: ", total_time_minutes)
+                        print("-----")
                     sys.exit()
         
 
-    def _get_namespace(self, element):
-        m = re.match(r'\{.*\}', element.tag)
-        return m.group(0) if m else ''
+    def _write_csv(self, headers, data):
+        output_filepath = os.path.join("data", self._output_filename)
+        with open(output_filepath, 'a+') as outfile:
+            headers = ','.join(headers)
+            outfile.write(headers + '\n')
+            for key,values in data.items():
+                output_line = key + ',' + ','.join(values.values())
+                outfile.write(output_line + '\n')

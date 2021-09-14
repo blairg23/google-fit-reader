a32fd47c7695b5921cc67d0ed38add29746ad28c
BlairG <blairg23@gmail.com>
2021-09-14 00:08:25 -0600

    Added pytz and dateutil as dependencies; ran pip-compile; and added method for translating timestamp from UTC to local timezone.

--- src/google_fit_reader/google_fit_reader.py
+++ src/google_fit_reader/google_fit_reader.py
@@ -4,6 +4,10 @@ import json
 import os
 import re
 import sys
+import pytz
+import datetime
+from dateutil import parser
+from dateutil.tz import gettz
 import xml.etree.ElementTree as ET
 
 
@@ -14,6 +18,7 @@ def main():
         directory=args.directory,
         file_type=args.file_type,
         output_filename=args.output_filename,
+        timezone=args.timezone,
         verbose=args.verbose,
     )
     google_fit_reader.parse()
@@ -39,7 +44,7 @@ def parse_args(args):
         "-f",
         type=str,
         default="json",
-        choices=['tcx', 'json'],
+        choices=["tcx", "json"],
         help="Specify the type of file to parse.",
     )
     parser.add_argument(
@@ -50,8 +55,15 @@ def parse_args(args):
         help="Specify the name of the output file.",
     )
     parser.add_argument(
-        '--verbose',
-        '-v',
+        "--timezone",
+        "-t",
+        type=str,
+        default="Americas/Denver",
+        help="Specify the timezone.",
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
         type=bool,
         default=False,
         choices=[True, False],
@@ -61,12 +73,13 @@ def parse_args(args):
 
 
 class GoogleFitReader:
-    def __init__(self, directory, file_type, output_filename, verbose=False):
+    def __init__(self, directory, file_type, output_filename, timezone, verbose=False):
         self._directory = directory
         self._file_type = file_type
         self._file_regex = "*." + file_type
         self._glob_path = os.path.join(self._directory, self._file_regex)
         self._output_filename = output_filename
+        self._timezone=timezone
         self._verbose = verbose
 
     def parse(self):
@@ -76,7 +89,10 @@ class GoogleFitReader:
             self._parse_xml()
 
     def _parse_json(self):
-        headers = ["timestamp", "activity", "distance (miles)", "duration (min)"]
+        """
+        Parses a JSON document containing Google Fit data and returns as CSV data.
+        """
+        headers = ["timestamp (utc)", f"timestamp ({self._timezone})", "activity", "distance (miles)", "duration (min)"]
         data = {}
 
         for filepath in glob.iglob(self._glob_path): 
@@ -84,6 +100,8 @@ class GoogleFitReader:
                 activity = json.load(infile)
             activity_name = activity.get("fitnessActivity")
             timestamp = activity.get("startTime")
+            print('timestamp: ', timestamp)
+            local_timestamp = self._translate_timestamp(timestamp, to_timezone=self._timezone)
             total_time_seconds = float(activity.get("duration").replace("s", ""))
             total_time_minutes = str(total_time_seconds / 60.0)
             aggregate = activity.get("aggregate")
@@ -95,6 +113,7 @@ class GoogleFitReader:
             if self._verbose:
                 print("filepath: ", filepath)
                 print("timestamp: ", timestamp)
+                print("local timestamp: ", local_timestamp)
                 print("activity: ", activity_name)
                 print("distance in miles: ", distance_miles)
                 print("total time in minutes: ", total_time_minutes)
@@ -104,6 +123,7 @@ class GoogleFitReader:
                 raise Exception(f"ERROR: Duplicate timestamps: {timestamp}")
             else:
                 data[timestamp] = {
+                    "local_timestamp": local_timestamp,
                     "activity": activity_name,
                     "distance": distance_miles,
                     "duration": total_time_minutes,
@@ -111,6 +131,9 @@ class GoogleFitReader:
         self._write_csv(headers=headers, data=data)
     
     def _parse_xml(self):
+        """
+        Parses an XML document containing Google Fit data and returns as CSV data.
+        """
         # for filename in os.listdir(self._directory):
         #     print("filename: ", filename)
         def get_namespace(element):
@@ -148,13 +171,31 @@ class GoogleFitReader:
                         print("total time in minutes: ", total_time_minutes)
                         print("-----")
                     sys.exit()
-        
+
+    def _translate_timestamp(self, timestamp, from_timezone=pytz.utc, to_timezone="America/Denver"):
+        """
+        :param str timestamp: A string containing the timestamp to translate.
+        :param str from_timezone: A string containing the timezone to translate from.
+        :param str to_timezone: A string containing the timezone to translate to.
+        """
+        old_datetime = parser.parse(timestamp)
+        new_datetime = old_datetime.astimezone(gettz(to_timezone)).strftime("%Y-%m-%dT%H:%M:%S:%f")
+        if self._verbose:
+            print("old datetime: ", old_datetime)
+            print("new datetime: ", new_datetime)
+        return new_datetime        
 
     def _write_csv(self, headers, data):
+        """
+        :param list headers: A list containing the headers for the CSV
+        :param dict data: A dict containing row data to write to CSV
+
+        Writes a CSV, given the headers and row data
+        """
         output_filepath = os.path.join("data", self._output_filename)
-        with open(output_filepath, 'a+') as outfile:
-            headers = ','.join(headers)
-            outfile.write(headers + '\n')
+        with open(output_filepath, "a+") as outfile:
+            headers = ",".join(headers)
+            outfile.write(headers + "\n")
             for key,values in data.items():
-                output_line = key + ',' + ','.join(values.values())
-                outfile.write(output_line + '\n')
+                output_line = key + "," + ",".join(values.values())
+                outfile.write(output_line + "\n")
